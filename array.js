ae6c7713a694b2d9a97e440efff2423ca106d69d
Bikal Koju <bikalkoju123@gmail.com>
2021-08-30 09:29:24 +0545

    first commit in array.js

---
+++ array.js
@@ -0,0 +1,18 @@
+const myFriends = ["bikal", "bishal", "sanish", "manish"];
+
+//prints the elements of array
+// console.log(myFriends);
+
+//returns the index of the array and prints it
+// const indexOfFriends = myFriends.length - 1;
+// console.log(indexOfFriends);
+
+//for in loop returns index of elements 
+// for (let elements in myFriends) {
+//     console.log(elements);
+// }
+
+//for of loop returns elements of the array
+// for (let elements of myFriends) {
+//     console.log(elements);
+// }
