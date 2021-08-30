62f3da0da761a39986b43e9ae1ab15042380a317
Bikal Koju <bikalkoju123@gmail.com>
2021-08-30 13:19:53 +0545

    added methods in array

--- array.js
+++ array.js
@@ -1,4 +1,24 @@
-const myFriends = ["bikal", "bishal", "sanish", "manish"];
+/*----------------array-------------*/
+
+/*--------------------------------
+----------------------------------
+Table of content
+
+# print array
+# index of array
+# for in loop
+# for of loop
+# array methods
+    #push() method
+    #unshift() method
+    #pop() method
+    #shift() method
+
+-------------------------------------
+-----------------------------------*/
+
+
+// const myFriends = ["bikal", "bishal", "sanish", "manish"];
 
 //prints the elements of array
 // console.log(myFriends);
@@ -16,3 +36,42 @@ const myFriends = ["bikal", "bishal", "sanish", "manish"];
 // for (let elements of myFriends) {
 //     console.log(elements);
 // }
+
+
+/*---- array methods ----*/
+
+//push() method
+//push method add element at the end of array and returns new length of the array 
+// const animals = ["cat", "dog", "rat"];
+// console.log(animals); //array before push method
+// const newAnimals = animals.push("cow"); 
+// console.log(newAnimals); // return value of the push method
+// console.log(animals); //array after push method
+
+//unshift method
+//unshift method add element at the front of the array
+// const veg = ["carrot", "tomato", "cabbage"];
+// console.log(veg);
+// const newVeg = veg.unshift('beans');
+// console.log(newVeg);
+// console.log(veg);
+
+//pop method
+//pop method removes the last element of the array and returns the removed element
+// const cars = ["BMW", "Porche", "Lamborghini"];
+// console.log(cars);
+// const newCars = cars.pop();
+// console.log(newCars);
+// console.log(cars);
+
+//shift method
+//shift method removes the first element of the array and returns the removed element
+// const brands = ["denim", "Lvd", "nike"];
+// console.log(brands);
+// const newBrands = brands.shift();
+// console.log(newBrands);
+// console.log(brands);
+
+//splice method
+
+
