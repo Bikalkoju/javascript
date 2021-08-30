cbb300ee6f7c7dbda61a37829f0c955a27809b35
Bikal Koju <bikalkoju123@gmail.com>
2021-08-30 12:24:12 +0545

    if_else.js and loop.js added

---
+++ if_else.js
@@ -0,0 +1,6 @@
+var num1 = 10, num2 = 20;
+if (num1 >= num2) {
+    console.log(num1 + " is greater than " + num2);
+} else {
+    console.log(num1 + " is smaller than " + num2);
+}
---
+++ loop.js
@@ -0,0 +1,20 @@
+//for loop
+var a = 4;
+for (let i = 1; i <= 10; i++){
+    var mul = a * i;
+    console.log(a + " * " + i + " = " + mul);
+}
+
+//while loop
+var num = 0;
+while (num <= 10) {
+    console.log(num);
+    num++;
+}
+
+// do-while loop
+var num = 0;
+do {
+    console.log(num);
+    num++;
+} while (num <= 10);
